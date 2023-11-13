4f5f231b767a833400b289e55ec23bc0beaf99bb
Shreerang Kolhe <96296671+iamShreerang@users.noreply.github.com>
2023-11-13 20:12:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,84 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const tableBody = document.querySelector('#userTable tbody');
-    const registrationForm = document.getElementById('registrationForm');
+let userForm = document.getElementById("user_form");
+let userEntries=[];
+if(localStorage.getItem("userEntries")===null)
+{
+    userEntries =[];
+    console.log("if");
+}
+else{
+   userEntries = JSON.parse(localStorage.getItem("userEntries")); 
+   console.log("else");
+}
+//console.log(userEntries);
 
-    // Function to add a row to the table and update storage
-    function addTableRow(data) {
-        const row = document.createElement('tr');
-        Object.values(data).forEach(value => {
-            const cell = document.createElement('td');
-            cell.textContent = value;
-            row.appendChild(cell);
-        });
-        tableBody.appendChild(row);
+let errors=[]
+const retieveEntries = ()=>{
+    let entries = localStorage.getItem('userEntries')
+    if(entries){
+        entries=JSON.parse(entries)
+    }else{
+        entries=[]
     }
+    return entries
+}
+const displayEntries = ()=>{
+let entries=retieveEntries()
+const tbleEntries = entries.map((entry)=>{
+const nameCell = `<td class='border px-4 py-2'>${entry.FullName}</td>`
+const emailCell = `<td class='border px-4 py-2'>${entry.email}</td>`
+const passwordCell = `<td class='border px-4 py-2'>${entry.password}</td>`
+const dobCell = `<td class='border px-4 py-2'>${entry.dob}</td>`
+const acceptTermsCell = `<td class='border px-4 py-2'>${entry.acceptTerms}</td>`
+const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`
+return row
 
-    // Load existing data from storage on page load
-    const storedData = JSON.parse(localStorage.getItem('userData')) || [];
-    // Display existing data in the table on page load
-    storedData.forEach(data => addTableRow(data));
+}).join('\n')
+const table =` <table class='table-auto w-full'>
+    <tr>
+    <th class='px-4 py-2 '>Name </th>
+    <th class='px-4 py-2 '>Email </th>
+    <th class='px-4 py-2 '>Password </th>
+    <th class='px-4 py-2 '>Dob </th>
+    <th class='px-4 py-2 '>Accepted terms? </th>
+    </tr>${tbleEntries}
+</table>`
+let details = document.getElementById('user-entries')
+details.innerHTML=table
+}
 
-    // Event listener for form submission
-    registrationForm.addEventListener('submit', function (event) {
-        // Prevent the default form submission behavior
-        event.preventDefault();
+const saveUserForm = (event)=>{
+event.preventDefault();
+const FullName = document.getElementById('name').value
+const email = document.getElementById('email').value
+const password = document.getElementById('password').value
+const dob = document.getElementById('dob').value
+const acceptTerms = document.getElementById('acceptTerms').checked
+var currentYear = new Date().getFullYear();
+var birthYear = dob.split("-");
+let year=birthYear[0]
+var age = currentYear-year
+console.log({age,currentYear,birthYear})
+if(age < 18 || age > 55){
+    document.getElementById('dob').style='border:1px solid red'
+  return  alert("Age must be between 18 and 55")
 
-        // Example: Get form values
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-        const dob = document.getElementById('dob').value;
-        const acceptTerms = document.getElementById('acceptTerms').checked;
+}else{
+    document.getElementById('dob').style='border:none'
 
-        // Validate email address
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(email)) {
-            alert('Invalid email address. Please enter a valid email.');
-            return;
-        }
-
-        // Validate age between 18 and 55
-        const dobDate = new Date(dob);
-        const currentDate = new Date();
-        const age = currentDate.getFullYear() - dobDate.getFullYear();
-
-        if (age < 18 || age >= 55) {
-            alert('Age must be between 18 (inclusive) and 55 (exclusive) years old.');
-            return;
-        }
-
-        // Add the form data to the table and update storage
-        addTableRow({
-            name: name,
-            email: email,
-            password: password,
-            dob: dob,
-            acceptTerms: acceptTerms ? 'Yes' : 'No'
-        });
-
-        // Update storage with the new data
-        storedData.push({
-            name: name,
-            email: email,
-            password: password,
-            dob: dob,
-            acceptTerms: acceptTerms
-        });
-        localStorage.setItem('userData', JSON.stringify(storedData));
-
-        // Optionally, you can clear the form fields after submission
-        registrationForm.reset();
-    });
-});
+    const entry ={
+        FullName,
+        email,
+        password,
+        dob,
+        acceptTerms
+     }
+     userEntries.push(entry);
+     localStorage.setItem("userEntries",JSON.stringify(userEntries))
+    displayEntries()
+    userForm.reset()
+   
+}
+ 
+}
+userForm.addEventListener('submit',saveUserForm)
+displayEntries()
