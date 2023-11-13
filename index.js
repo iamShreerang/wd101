80f063c301ad59773d93d5e3841da10b2a285c3a
Shreerang Kolhe <96296671+iamShreerang@users.noreply.github.com>
2023-11-13 20:15:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,84 +1,109 @@
-let userForm = document.getElementById("user_form");
-let userEntries=[];
-if(localStorage.getItem("userEntries")===null)
-{
-    userEntries =[];
-    console.log("if");
-}
-else{
-   userEntries = JSON.parse(localStorage.getItem("userEntries")); 
-   console.log("else");
-}
-//console.log(userEntries);
+///new
+let recieve = () => {
+  let values = localStorage.getItem("user-data");
+  if (values) {
+    values = JSON.parse(values);
+  } else {
+    values = [];
+  }
+  return values;
+};
 
-let errors=[]
-const retieveEntries = ()=>{
-    let entries = localStorage.getItem('userEntries')
-    if(entries){
-        entries=JSON.parse(entries)
-    }else{
-        entries=[]
+let data = recieve();
+
+let disp = () => {
+  const values = recieve();
+  let tablevel = values
+    .map((valx) => {
+      let namet = `<td>${valx.name}</td>`;
+      let emailt = `<td>${valx.mail}</td>`;
+      let passwdt = `<td>${valx.passwd}</td>`;
+      let dateofbirtht = `<td>${valx.dateofbirth}</td>`;
+      let termsandcondt = `<td>${valx.termsandcond}</td>`;
+      let row = `<tr>${namet} ${emailt} ${passwdt} ${dateofbirtht} ${termsandcondt}</tr>`;
+      return row;
+    })
+    .join("\n");
+
+  let table = `<table>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Dob</th>
+            <th>Accepted Terms?</th>
+        </tr>${tablevel}
+    </table>`;
+  let det = document.getElementById("x-data");
+  det.innerHTML = table;
+};
+
+let checkage = (valueofage) => {
+  let x;
+  if (valueofage >= 18 && valueofage <= 55) {
+    x = true;
+  } else {
+    alert("You are not in valid age it ranges between 15 to 55");
+    x = false;
+  }
+  return x;
+};
+
+let mustcheck = (passwd) => {
+  let letters = /^[A-Z]+$/;
+  let num = /^[0-9]+$/;
+  let sym = /^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+  let x;
+
+  if (passwd.length < 6) {
+    alert("It should contain more than 6 characters");
+    x = false;
+  }
+  if (passwd.length > 6) {
+    if (passwd.match(letters)) {
+      alert("It should contain any Uppercase letter");
+      x = false;
+    } else if (passwd.match(num) | passwd.match(sym)) {
+      alert("It should contain any number or special character");
+      x = false;
+    } else {
+      x = true;
     }
-    return entries
-}
-const displayEntries = ()=>{
-let entries=retieveEntries()
-const tbleEntries = entries.map((entry)=>{
-const nameCell = `<td class='border px-4 py-2'>${entry.FullName}</td>`
-const emailCell = `<td class='border px-4 py-2'>${entry.email}</td>`
-const passwordCell = `<td class='border px-4 py-2'>${entry.password}</td>`
-const dobCell = `<td class='border px-4 py-2'>${entry.dob}</td>`
-const acceptTermsCell = `<td class='border px-4 py-2'>${entry.acceptTerms}</td>`
-const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`
-return row
+    return x;
+  }
+};
 
-}).join('\n')
-const table =` <table class='table-auto w-full'>
-    <tr>
-    <th class='px-4 py-2 '>Name </th>
-    <th class='px-4 py-2 '>Email </th>
-    <th class='px-4 py-2 '>Password </th>
-    <th class='px-4 py-2 '>Dob </th>
-    <th class='px-4 py-2 '>Accepted terms? </th>
-    </tr>${tbleEntries}
-</table>`
-let details = document.getElementById('user-entries')
-details.innerHTML=table
-}
+let form = document.getElementById("form");
 
-const saveUserForm = (event)=>{
-event.preventDefault();
-const FullName = document.getElementById('name').value
-const email = document.getElementById('email').value
-const password = document.getElementById('password').value
-const dob = document.getElementById('dob').value
-const acceptTerms = document.getElementById('acceptTerms').checked
-var currentYear = new Date().getFullYear();
-var birthYear = dob.split("-");
-let year=birthYear[0]
-var age = currentYear-year
-console.log({age,currentYear,birthYear})
-if(age < 18 || age > 55){
-    document.getElementById('dob').style='border:1px solid red'
-  return  alert("Age must be between 18 and 55")
+let savedata = (op) => {
+  op.preventDefault();
+  let name = document.getElementById("name").value;
+  let mail = document.getElementById("email").value;
+  let passwd = document.getElementById("password").value;
+  let dateofbirth = document.getElementById("dob").value;
+  let termsandcond = document.getElementById("check").checked;
 
-}else{
-    document.getElementById('dob').style='border:none'
+  //age validation
+  let d = new Date(dateofbirth);
+  let time = Date.now() - d.getTime();
+  let age = new Date(time);
+  let tval = Math.abs(age.getUTCFullYear() - 1970);
+  let m = mustcheck(passwd);
+  let cage = checkage(tval);
 
-    const entry ={
-        FullName,
-        email,
-        password,
-        dob,
-        acceptTerms
-     }
-     userEntries.push(entry);
-     localStorage.setItem("userEntries",JSON.stringify(userEntries))
-    displayEntries()
-    userForm.reset()
-   
-}
- 
-}
-userForm.addEventListener('submit',saveUserForm)
-displayEntries()
+  if (m === true && cage === true) {
+    const valx = {
+      name,
+      mail,
+      passwd,
+      dateofbirth,
+      termsandcond,
+    };
+    data.push(valx);
+    localStorage.setItem("user-data", JSON.stringify(data));
+    disp();
+  }
+};
+
+form.addEventListener("submit", savedata);
+disp();
